52239f7e9713290d20726ea38c2df838c954e3f5
ptngmbjr <31225723+ptngmbjr@users.noreply.github.com>
2018-10-09 18:04:34 +0530

    App tour page done
    Splash screen done
    App tour page done

--- app/src/main/java/com/baseschoolapp/schoolapp/MainActivity.java
+++ app/src/main/java/com/baseschoolapp/schoolapp/MainActivity.java
@@ -1,13 +1,75 @@
 package com.baseschoolapp.schoolapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener {
+
+    private SliderLayout mDemoSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+        mDemoSlider = (SliderLayout) findViewById(R.id.slider);
+
+        mDemoSlider.setCustomIndicator((PagerIndicator) findViewById(R.id.custom_indicator));
+
+        HashMap<String, Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("welcome", R.drawable.welcome);
+        file_maps.put("track_school_bus", R.drawable.track_school_bus);
+        file_maps.put("attendance", R.drawable.attendance);
+        file_maps.put("student_activities", R.drawable.student_activities);
+
+        for (String name : file_maps.keySet()) {
+            DefaultSliderView textSliderView = new DefaultSliderView(this);
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name));
+            mDemoSlider.addSlider(textSliderView);
+        }
+        mDemoSlider.setDuration(400);
+
+    }
+
+    @Override
+    protected void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
     }
 }
