98d1d0090f0041ceafb17fdb4a1f731844c24332
ptngmbjr <31225723+ptngmbjr@users.noreply.github.com>
2018-10-12 18:00:54 +0530

    OTP Screen done
    otp screen with timer done

---
+++ app/src/main/java/com/baseschoolapp/schoolapp/Otp.java
@@ -0,0 +1,125 @@
+package com.baseschoolapp.schoolapp;
+
+import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Otp extends AppCompatActivity {
+
+    TextView txt_otp_text, txt_resend_otp;
+
+    private long startTime = 0L;
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+    private TextView timerValue;
+    EditText txtOTP;
+
+    private int MAX_TIMER = 10;
+
+    private Handler customHandler = new Handler();
+
+    Button btnSubmitOtp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+
+        String phoneNo = getIntent().getExtras().getString("phone_no");
+        String otpText = getString(R.string.otp_text, formatMobileNo(phoneNo));
+
+        txt_otp_text = (TextView) findViewById(R.id.txtvw_otp_text);
+        timerValue = (TextView) findViewById(R.id.txt_timer);
+        txtOTP = (EditText) findViewById(R.id.edtxt_otp);
+        txt_resend_otp = (TextView) findViewById(R.id.txt_resend_otp);
+
+        btnSubmitOtp = (Button) findViewById(R.id.btn_submit_otp);
+
+        txt_otp_text.setText(otpText);
+
+        GradientDrawable background = (GradientDrawable) btnSubmitOtp.getBackground();
+        background.setColor(getResources().getColor(R.color.loginblue));
+
+        txtOTP.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+
+                InputMethodManager imm = (InputMethodManager) getApplicationContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        startTimer();
+        enableDisableResend(false);
+
+    }
+
+    public String formatMobileNo(String no) {
+        no = no.trim();
+        if (no.length() >= 10)
+            return no.substring(0, (no.length() / 2) - 1) + "xxx" + no.substring((no.length() / 2) + 2, no.length());
+        return no;
+    }
+
+    public void startTimer() {
+        startTime = SystemClock.uptimeMillis();
+        customHandler.postDelayed(updateTimerThread, 0);
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+
+        public void run() {
+
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+
+            int disp = MAX_TIMER - secs;
+
+            timerValue.setText("" + mins + ":"
+                    + String.format("%02d", disp));
+
+
+            customHandler.postDelayed(this, 0);
+
+            if (disp == 0)
+                stopTimer();
+        }
+
+    };
+
+    private void stopTimer() {
+        customHandler.removeCallbacks(updateTimerThread);
+        enableDisableResend(true);
+    }
+
+    private void enableDisableResend(boolean enable) {
+        if (enable)
+            txt_resend_otp.setTextColor(ContextCompat.getColor(this, R.color.loginblue));
+        else
+            txt_resend_otp.setTextColor(ContextCompat.getColor(this, R.color.disabled));
+    }
+}
--- app/src/main/java/com/baseschoolapp/schoolapp/Signup.java
+++ app/src/main/java/com/baseschoolapp/schoolapp/Signup.java
@@ -1,13 +1,26 @@
 package com.baseschoolapp.schoolapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
 
+    public final int SUCCESS = 0;
+    public final int INVALID_PHONE_NO = -1;
+
     private Button btnSignUpGetStarted, btnSignUpLogin;
+
+    private EditText txt_phone_number;
+    TextView txt_error_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +29,97 @@ public class Signup extends AppCompatActivity {
 
         btnSignUpGetStarted = (Button) findViewById(R.id.btnsignupgetstarted);
         btnSignUpLogin = (Button) findViewById(R.id.btn_sign_up_login);
+        txt_phone_number = (EditText) findViewById(R.id.edtxt_regmobileno);
+        txt_error_message = (TextView) findViewById(R.id.txt_error);
+
+        txt_error_message.setVisibility(View.GONE);
+        txt_phone_number.setImeActionLabel("Done", KeyEvent.KEYCODE_ENTER);
+
+        txt_phone_number.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+
+                InputMethodManager imm = (InputMethodManager) getApplicationContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    getStartedWithMobile(v);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
 
         GradientDrawable background = (GradientDrawable) btnSignUpGetStarted.getBackground();
-        background.setColor(getResources().getColor(R.color.loginblue));
+        background.setColor(
+
+                getResources().
+
+                        getColor(R.color.loginblue));
 
         background = (GradientDrawable) btnSignUpLogin.getBackground();
-        background.setColor(getResources().getColor(R.color.signuporange));
+        background.setColor(
+
+                getResources().
+
+                        getColor(R.color.signuporange));
 
     }
+
+    public void login(View view) {
+        finish();
+    }
+
+    public void getStartedWithMobile(View view) {
+
+        txt_error_message.setVisibility(View.GONE);
+
+        int res = -1;
+        res = isPhoneNoValid(txt_phone_number.getText().toString());
+
+        if (res == SUCCESS)
+            getStarted();
+        else
+            handleError(res);
+    }
+
+    public void getStarted() {
+        Intent intent = new Intent(this, Otp.class);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("phone_no", txt_phone_number.getText().toString());
+
+        intent.putExtras(bundle);
+        startActivity(intent);
+
+    }
+
+    public int isPhoneNoValid(String phoneNo) {
+        int res = INVALID_PHONE_NO;
+
+        phoneNo = phoneNo.trim();
+
+        if (!phoneNo.isEmpty())
+            res = SUCCESS;
+
+        return res;
+
+    }
+
+    public void handleError(int errorNo) {
+        String res = "";
+
+        switch (errorNo) {
+            case INVALID_PHONE_NO:
+                res = "Please enter a valid phone number!!";
+                break;
+            default:
+                break;
+        }
+
+        txt_error_message.setVisibility(View.VISIBLE);
+        txt_error_message.setText("*" + res);
+
+    }
+
 }
